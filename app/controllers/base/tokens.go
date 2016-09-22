1b64f9ea7a373ed25541563b37414d3045c0850e
adrianvana <adrian.v.navarrete@gmail.com>
2016-09-22 13:06:26 -0500

    Se quitaron los espacios en los strings de header y payload de los tokens

--- app/controllers/base/tokens.go
+++ app/controllers/base/tokens.go
@@ -16,10 +16,10 @@ func CreateToken(iss string, name string) string{
 	/* 	Función que recopila la información del usuario y lo codifica para formar el token del tipo JWT.
 		Recibe el nombre de usuario (nickname) y el nombre completo del usuario para codificarlos dentro del token */
 
-	header := []byte(`{"typ": "JWT", "alg": "HS256"}`)
+	header := []byte(`{"alg":"HS256","typ":"JWT"}`)
 
 	exp := int(time.Now().Unix()) + 86400    // Tiempo de expiración del token más 1 día
-	payload := []byte(`{"iss": "`+iss+`", "exp": "`+strconv.Itoa(exp)+`", "name": "`+name+`"}`)
+	payload := []byte(`{"iss":"`+iss+`","exp":`+strconv.Itoa(exp)+`,"name":"`+name+`"}`)
 
 	signature := base64.StdEncoding.EncodeToString(header)+"."+base64.StdEncoding.EncodeToString(payload)
 
@@ -33,16 +33,19 @@ func CheckToken(token string) bool {
 	/* Función que verifica la expliración del token y retorna true si es válido o false en caso contrario */
 
 	tokSplit := strings.Split(token, ".")
-	payloadJson, _ := base64.StdEncoding.DecodeString(tokSplit[1])
+	payloadJson, _ := base64.StdEncoding.DecodeString(tokSplit[1]) // Divide el token y extrae la parte de payload
 
 	type Payload struct {
-		Iss, Name, Exp string
+		Iss  	string   	`json:"iss"`
+		Name 	string   	`json:"name"`// Se almacenarán los datos del payload
+		Exp 	int 		`json:"exp"`
 	}
 
 	pl := Payload{}
 	json.Unmarshal(payloadJson, &pl)
-	exp, _ := strconv.Atoi(pl.Exp) // Convierte string a int
-	if exp < int(time.Now().Unix()) {  
+	// exp, _ := strconv.Atoi(pl.Exp) // Convierte string a int
+
+	if pl.Exp < int(time.Now().Unix()) {  
 		return false
 	} else {
 		return true
