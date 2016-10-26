c7fd13d9576ef4580e72bfdb590f62a5571f35d6
adrianvana <adrian.v.navarrete@gmail.com>
2016-10-26 13:29:51 -0500

    Agregar/editar ID de proveedor en los productos del almacén

--- app/models/modAlmacen.go
+++ app/models/modAlmacen.go
@@ -1,11 +1,14 @@
 package models
 
+import "gopkg.in/mgo.v2/bson"
+
 type Product struct {
-	Name 			string	`json:"name"`
-	Type 			string 	`json:"type"`
-	Description 	string	`json:"description"`
-	Quantity 		int 	`json:"quantity"`
-	Min_quantity 	int		`json:"min_quantity"`
-	Sale			bool	`json:"sale"`
-	N_serial 		string 	`json:"n_serial"`
+	Name 			string			`json:"name"`
+	Type 			string 			`json:"type"`
+	Description 	string			`json:"description"`
+	Quantity 		int 			`json:"quantity"`
+	Min_quantity 	int				`json:"min_quantity"`
+	Sale			bool			`json:"sale"`
+	N_serial 		string 			`json:"n_serial"`
+	Caterer			bson.ObjectId 	`json:"caterer" bson:"caterer,omitempty"`
 }
